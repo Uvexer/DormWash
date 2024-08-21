71d4d95255d01213f85f58c09b9ff84236d55ca0
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-08-21 23:45:53 +0300

    Initial Commit

---
+++ DormWash/DormWashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormWashApp.swift
+//  DormWash
+//
+//  Created by bodya on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DormWashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
